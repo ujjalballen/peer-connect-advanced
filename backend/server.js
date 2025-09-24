f85c2b2ebe60c30ab63cec66e68b1779e5d0ad15
Ujjal B. Allen <ujjal.vision02@gmail.com>
2025-09-25 04:22:14 +0600

    setup the server

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
+const app = express();
+const port = process.env.PROT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173", // your frontend origin
+    methods: ["GET", "POST"],
+    // credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Socket ID: ", socket.id);
+});
+
+httpServer.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
+});
